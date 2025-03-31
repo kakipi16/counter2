aa15bfd222506b099e4cd0c7f9f665a4b38b7da4
kakipi16 <kakipi533@gmail.com>
2025-03-31 21:18:41 +0900

    imgAdd

--- counter.js
+++ counter.js
@@ -5,8 +5,22 @@
         let currentCount = parseInt($counter.textContent);
         if($targetButton.textContent === "+") {
             $counter.textContent = currentCount + 1;
-        } else {
+        } else if($targetButton.textContent === "-") {
             $counter.textContent = currentCount - 1;
+        } else if($targetButton.textContent === "+10") {
+            $counter.textContent = currentCount + 10;
+        } else if($targetButton.textContent === "-10") {
+            $counter.textContent = currentCount - 10;
+        } else if($targetButton.textContent === "multiply") {
+            $counter.textContent = Math.pow(currentCount, 2);
+        }
+        if($counter.textContent >= 10000) {
+            document.querySelector(".js-image").classList.add('isActive');
+        } else if($counter.textContent <= -50) {
+            document.querySelector(".js-image2").classList.add('isActive2');
+        } else {
+            document.querySelector(".js-image").classList.remove('isActive');
+            document.querySelector(".js-image2").classList.remove('isActive2');
         }
     }
 
